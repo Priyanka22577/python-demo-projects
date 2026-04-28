7f3519211d0e8566da375a918784054faa4165a8
Priyanka22577 <priyankachauhan.959124@gmail.com>
2026-04-28 09:39:41 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,17 @@
+num1 = 10
+num2 = 10
+op = '+'
+
+if op == "+":
+    print("Result:", num1 + num2)
+elif op == "-":
+    print("Result:", num1 - num2)
+elif op == "*":
+    print("Result:", num1 * num2)
+elif op == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Cannot divide by zero")
+else:
+    print("Invalid operation")
